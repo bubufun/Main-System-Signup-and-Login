26ff7750cbd1ba2ea34797989753718096b3567b
bubufun <bitterbu@gmail.com>
2021-01-29 21:45:12 +0800

    logout edit

--- new_logout.py
+++ new_logout.py
@@ -17,25 +17,30 @@ def logout_check():
         type = result['type']
         time = result['Time']
         log_check = (Name,type,time)
+        
+    try:
 
-    if log_check[1] == 'logout':
-        conn = db_created.db_connect_host()
-        cursor = conn.cursor()
-        sql = 'SELECT * FROM login_check where memberid = "%s"' % log_check[0]
-        cursor.execute(sql)
-        info = cursor.fetchall()
-        savetime =datetime.strptime(log_check[2].replace('/','-'), "%Y-%m-%d %H:%M:%S")
+        if log_check[1] == 'logout':
+            conn = db_created.db_connect_host()
+            cursor = conn.cursor()
+            sql = 'SELECT * FROM login_check where memberid = "%s"' % log_check[0]
+            cursor.execute(sql)
+            info = cursor.fetchall()
+            savetime =datetime.strptime(log_check[2].replace('/','-'), "%Y-%m-%d %H:%M:%S")
 
-        if info:
-            cursor.close()
-            conn.close()
-            db_created.logout(info[0][0],log_check[0],savetime)
+            if info:
+                cursor.close()
+                conn.close()
+                db_created.logout(info[0][0],log_check[0],savetime)
 
-            return 1
+                return 1
 
-        elif not info:
+            elif not info:
 
-            return 0
+                return 0
+    except:
+            
+        return -1
 
 if __name__ == '__main__':
     a=logout_check()
